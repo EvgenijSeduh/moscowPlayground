3a2a1d6d5b59a0d876104aec8e767fcfcf551ec1
evgenij <evgenijseduh@gmail.com>
2024-03-10 16:40:16 +0300

    сделан успешный запрос из index.js можно разгоняться

--- js/app.js
+++ js/app.js
@@ -1,39 +1,37 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "sports_grounds",
-    password: ""
-});
-
-connection.connect(err => {
-    if (err) {
-        return err;
-    }
-    else {
-        console.log('Connrction on');
-    }
-});
-
-let query = "SELECT * FROM name_columns;";
-connection.query(query, (err, result, fields) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(result[2]['ru_name']);
-        console.log(fields);
-    }
-});
+const NAME_DB = "sports_grounds";
+const PASSWORD_DB = "";
+const USER_DB = "root";
+const HOST_DB = "localhost";
 
 
 
-connection.end(err => {
-    if (err) {
-        console.log(err);
-        return err;
-    }
-    else {
-        console.log("Connection off");
-    }
-});
+function requestDB(query, callback) {
+    const connection = mysql.createConnection({
+      host: HOST_DB,
+      user:USER_DB,
+      password: PASSWORD_DB,
+      database: NAME_DB
+    });
+  
+    connection.connect((err) => {
+      if (err) {
+        console.error('Ошибка подключения к базе данных: ', err);
+        return callback(err, null);
+      }
+  
+      connection.query(query, (err, result) => {
+        if (err) {
+          console.error('Ошибка выполнения запроса к базе данных: ', err);
+          connection.end(); // Закрытие соединения
+          return callback(err, null);
+        }
+  
+        connection.end(); // Закрытие соединения
+        callback(null, result);
+      });
+    });
+  }
+  
+  module.exports = requestDB;
---
+++ js/info_playgrounds_tb.js
@@ -0,0 +1,173 @@
+// const INFO_PLAYGROUND_NAME = "sports_grounds";
+
+// const ID = {ru:"global_id",en:"global_id"};
+// const OBJECT_NAME = {ru:"Название спортивного объекта",en:"ObjectName"};
+// const NAME_WINTER = {ru:"Название спортивной зоны в зимний период",en:"NameWinter"};
+// const PHOTO_WINTER = {ru:"Фотография в зимний период",en:"PhotoWinter"};
+// const ADM_AREA = {ru:"Административный округ",en:"AdmArea"};
+// const DISTRICT = {ru:"Район",en:"District"};
+// const ADDRESS = {ru:"Адрес",en:"Address"};
+// const EMAIL = {ru:"Адрес электронной почты",en:"Email"};
+// const WEBSITE = {ru:"Адрес сайта",en:"WebSite"};
+// const HELP_PHONE = {ru:"Справочный телефон",en:"HelpPhone"};
+// const HELP_PHONE_EXTENSION = {ru:"Добавочный номер",en:"HelpPhoneExtension"};
+// const WORKING_HOURS_WINTER = {ru:"График работы в зимний период",en:"WorkingHoursWinter"};
+// const CLARIFICATION_OF_WORKING_HOURS_WINTER = {ru:"Уточнение графика работы в зимний период",en:"ClarificationOfWorkingHoursWinter"};
+// const HAS_EQUIPMENT_RENTAL = {ru:"Возможность проката оборудования",en:"HasEquipmentRental"};
+// const EQUIPMENANR_RENTAL_COMMENTS = {ru:"Комментарии для проката оборудования",en:"EquipmentRentalComments"};
+// const HAS_TECH_SERVICE_T = {ru:"Наличие сервиса технического обслуживания",en:"HasTechService"};
+// const TECH_SERVICE_COMMENTS = {ru:"Комментарии для сервиса технического обслуживания",en:"TechServiceComments"};
+// const HAS_DRESSING_ROOM = {ru:"Наличие раздевалки",en:"HasDressingRoom"};
+// const HAS_EATERY = {ru:"Наличие точки питания",en:"HasEatery"};
+// const HAS_TOILET = {ru:"Наличие туалета",en:"HasToilet"};
+// const HAS_WIFI = {ru:"Наличие Wi-Fi",en:"HasWifi"};
+// const HAS_CASH_MACHINE = {ru:"Наличие банкомата",en:"HasCashMachine"};
+// const HAS_FIRST_AID_POST = {ru:"Наличие медпункта",en:"HasFirstAidPost"};
+// const HAS_MUSIC = {ru:"Наличие звукового сопровождения",en:"HasMusic"};
+// const USAGE_PERIOD_WINTER = {ru:"Период эксплуатации в зимний период",en:"UsagePeriodWinter"};
+// const DIMENSIONS_WINTER = {ru:"Размеры в зимний период",en:"DimensionsWinter"};
+// const LIGHTING = {ru:"Освещение",en:"Lighting"};
+// const SURFACE_TYPE_WINTER = {ru:"Покрытие в зимний период",en:"SurfaceTypeWinter"};
+// const SEATS = {ru:"Количество оборудованых посадочных мест",en:"Seats"};
+// const PAID = {ru:"Форма посещения(платность)",en:"Paid"};
+// const PAID_COMMENTS = {ru:"Комментарии к стоимости посещения",en:"PaidComments"};
+// const DISABILITY_FRENDLY = {ru:"Приспособленость для занятий инвалидов",en:"DisabilityFriendly"};
+// const SERVICES_WINTER = {ru:"Услуги предоставляемые в зимний период",en:"ServicesWinter"};
+
+
+module.exports = {
+    INFO_PLAYGROUND_NAME: "info_playground",
+    ID: {
+      ru: "global_id",
+      en: "global_id"
+    },
+    OBJECT_NAME: {
+      ru: "Название спортивного объекта",
+      en: "ObjectName"
+    },
+    NAME_WINTER: {
+      ru: "Название спортивной зоны в зимний период",
+      en: "NameWinter"
+    },
+    PHOTO_WINTER: {
+      ru: "Фотография в зимний период",
+      en: "PhotoWinter"
+    },
+    ADM_AREA: {
+      ru: "Административный округ",
+      en: "AdmArea"
+    },
+    DISTRICT: {
+      ru: "Район",
+      en: "District"
+    },
+    ADDRESS: {
+      ru: "Адрес",
+      en: "Address"
+    },
+    EMAIL: {
+      ru: "Адрес электронной почты",
+      en: "Email"
+    },
+    WEBSITE: {
+      ru: "Адрес сайта",
+      en: "WebSite"
+    },
+    HELP_PHONE: {
+      ru: "Справочный телефон",
+      en: "HelpPhone"
+    },
+    HELP_PHONE_EXTENSION: {
+      ru: "Добавочный номер",
+      en: "HelpPhoneExtension"
+    },
+    WORKING_HOURS_WINTER: {
+      ru: "График работы в зимний период",
+      en: "WorkingHoursWinter"
+    },
+    CLARIFICATION_OF_WORKING_HOURS_WINTER: {
+      ru: "Уточнение графика работы в зимний период",
+      en: "ClarificationOfWorkingHoursWinter"
+    },
+    HAS_EQUIPMENT_RENTAL: {
+      ru: "Возможность проката оборудования",
+      en: "HasEquipmentRental"
+    },
+    EQUIPMENANR_RENTAL_COMMENTS: {
+      ru: "Комментарии для проката оборудования",
+      en: "EquipmentRentalComments"
+    },
+    HAS_TECH_SERVICE_T: {
+      ru: "Наличие сервиса технического обслуживания",
+      en: "HasTechService"
+    },
+    TECH_SERVICE_COMMENTS: {
+      ru: "Комментарии для сервиса технического обслуживания",
+      en: "TechServiceComments"
+    },
+    HAS_DRESSING_ROOM: {
+      ru: "Наличие раздевалки",
+      en: "HasDressingRoom"
+    },
+    HAS_EATERY: {
+      ru: "Наличие точки питания",
+      en: "HasEatery"
+    },
+    HAS_TOILET: {
+      ru: "Наличие туалета",
+      en: "HasToilet"
+    },
+    HAS_WIFI: {
+      ru: "Наличие Wi-Fi",
+      en: "HasWifi"
+    },
+    HAS_CASH_MACHINE: {
+      ru: "Наличие банкомата",
+      en: "HasCashMachine"
+    },
+    HAS_FIRST_AID_POST: {
+      ru: "Наличие медпункта",
+      en: "HasFirstAidPost"
+    },
+    HAS_MUSIC: {
+      ru: "Наличие звукового сопровождения",
+      en: "HasMusic"
+    },
+    USAGE_PERIOD_WINTER: {
+      ru: "Период эксплуатации в зимний период",
+      en: "UsagePeriodWinter"
+    },
+    DIMENSIONS_WINTER: {
+      ru: "Размеры в зимний период",
+      en: "DimensionsWinter"
+    },
+    LIGHTING: {
+      ru: "Освещение",
+      en: "Lighting"
+    },
+    SURFACE_TYPE_WINTER: {
+      ru: "Покрытие в зимний период",
+      en: "SurfaceTypeWinter"
+    },
+    SEATS: {
+      ru: "Количество оборудованых посадочных мест",
+      en: "Seats"
+    },
+    PAID: {
+      ru: "Форма посещения(платность)",
+      en: "Paid"
+    },
+    PAID_COMMENTS: {
+      ru: "Комментарии к стоимости посещения",
+      en: "PaidComments"
+    },
+    DISABILITY_FRENDLY: {
+      ru: "Приспособленость для занятий инвалидов",
+      en: "DisabilityFriendly"
+    },
+    SERVICES_WINTER: {
+      ru: "Услуги предоставляемые в зимний период",
+      en: "ServicesWinter"
+    }
+  };
+  
